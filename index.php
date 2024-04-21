73e4ab79cd39a94f755f4a27370356f8ecf414c7
YanGds99 <99548419+YanGds99@users.noreply.github.com>
2024-04-21 19:29:49 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,66 +1,29 @@
-<?php
-
-    if(isset($_POST['submit']))
-    {
-        include_once('config.php');
-        
-        $numero = $_POST['numero'];
-        $natureza = $_POST['natureza'];
-        $empresa = $_POST['empresa'];
-        $uf = $_POST['uf'];
-        $dataemissao = $_POST['dataemissao'];
-        $datavencimento = $_POST['datavencimento'];
-
-        $result = mysqli_query($conexao, "INSERT INTO notas_fiscais(Numero,Natureza,Empresa,Uf,Data_emissao,Data_vencimento)
-        VALUES ('$numero','$natureza','$empresa','$uf','$dataemissao','$datavencimento')");
-    }
-
-?>
-
 <!DOCTYPE html>
-
 <html>
-
     <head>
-        <title>Página Inicial</title>
+    <title>Página Inicial</title>
         <meta charset="UTF-8">
         <link rel="shortcut icon" href="imagens/nfe.png">
-
+        <link rel="stylesheet" tipe="text/css" href="estilos.css">
     </head>
-
     <body>
-        <h1>Cadastrar Notas Fiscais</h1>
+        <header id="consulta">
+            <img id=“img-logo” src="imagens/nfe.png">
+        </header>
 
-        <form action="index.php" method="post">
+        <main>
+            <a>Bem vindo ao seu sistema de Cadastro de Notas Fiscais</a><br><br>
+            <a>Aqui você pode cadastrar as informações de suas notas, e também realizar a consulta das mesmas de diversas formas.</a><br><br>
+            
+            <fieldset>
+                <h1>Selecione o serviço desejado</h1>
+                <a id="link" href="cadastro.php">Cadastrar Notas Fiscais</a>
+                <a id="link" href="consultaPorNumero.php">Consultar por Número</a><br>
+            </fieldset>
+        </main>
 
-        <fieldset>
-            <legend> Cadastro da Nota Fiscal </legend>
+        <footer>
 
-            <label for="numero">Número</label>: <br>
-            <input type="number" name="numero" id="numero"> <br>
-
-            <label for="natureza">Natureza</label>: <br>
-            <input type="text" name="natureza" id="natureza"> <br>
-
-            <label for="empresa">Empresa</label>: <br>
-            <input type="text" name="empresa" id="empresa"> <br>
-
-            <label for="uf">Cidade/Estado</label>: <br>
-            <input type="uf" name="uf" id="uf"> <br>
-
-            <label for="dataemissao">Data de Emissão</label>: <br>
-            <input type="date" name="dataemissao" id="dataemissao"> <br>
-
-            <label for="datavencimento">Data de Vencimento</label>: <br>
-            <input type="date" name="datavencimento" id="datavencimento"> <br>
-
-            <br><button type="reset">Limpar</button>
-            <input type="submit" name="submit" id="submit">
-
-        </fieldset>
-
-        </form>
-
+        </footer>
     </body>
-
 </html>
